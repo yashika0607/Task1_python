7c4d5887d4aac4307e2f6ad7819bb86eef1b79c1
yashika0607 <63790665+yashika0607@users.noreply.github.com>
2021-02-14 18:52:04 +0530

    Create first_task.py

---
+++ first_task.py
@@ -0,0 +1,9 @@
+#task 1
+r=float(input("Enter the radius of the circle?\n"))
+pi=3.143
+area=pi*r*r
+print("Area of the circle is ",area)
+
+#task 2
+x=input("Enter the file name\n")
+print(x+".py")
