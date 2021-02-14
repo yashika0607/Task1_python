d9260f1d9dd452eadbcfeaf2016305af7b73caa0
yashika0607 <63790665+yashika0607@users.noreply.github.com>
2021-02-14 19:11:01 +0530

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,12 @@
+NumList = []
+
+Number = int(input("Please enter the Total Number of List Elements: "))
+for i in range(1, Number + 1):
+    value = int(input("Please enter the Value of %d Element : " %i))
+    NumList.append(value)
+print(NumList)
+
+print("\nPositive Numbers in this List are : ")
+for j in range(Number):
+    if(NumList[j] >= 0):
+        print(NumList[j], end = '   ')
